abae14cc779bba8b6705f5560be4e139c3fbbd29
Nicolas <bernalmedina.n@gmail.com>
2021-10-14 13:23:09 -0500

    Fin Seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { ContadorModule } from './Contador/Contador.module';
 import { HeroesModule } from './Heroes/Heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  constructor(){
+
+  }
+
+  nuevo: Personaje = {
+    nombre: "Trunks",
+    poder: 14000
+  }
+
+  /*
+  get personajes(): Personaje[] {
+    return this.DbzService.personajes;
+  }
+  */
+
+  agregarNuevoPersonaje( argumento: Personaje ){
+    //this.personajes.push(argumento);
+
+  }
+
+
+
+}
